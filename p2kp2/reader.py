04874ff08f5ce9f3c0fa2530b32a1aa0494a4432
Stephen Hassard <steve@hassard.net>
2021-04-11 13:38:56 -0700

    Add login username field

--- p2kp2/reader.py
+++ p2kp2/reader.py
@@ -131,7 +131,7 @@ class PassEntry:
         for key, value in data:
             if key == "url":
                 self.url = value
-            elif key == "user":
+            elif key == "user" or key == "login":
                 self.user = value
             elif key == "notes":
                 self.notes = value
